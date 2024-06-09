2812ba3917ff7e822a45807e8874d764096b1634
Павел Сотченков <dr.sot4enkoff2017@yandex.ru>
2024-06-09 21:53:15 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+tasks_complete = 12
+time_spent = 1.5
+class_name = 'Python'
+time_for_task = (time_spent/tasks_complete)
+print(f'Курс: {class_name}, всего задач: {tasks_complete}, затрачено часов: {time_spent}, '
+      f'среднее время выполнения: {time_for_task}')
